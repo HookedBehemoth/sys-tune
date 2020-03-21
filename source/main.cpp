13f4eccb162e48d0a7592aacb26e9a911ce2f47c
HookedBehemoth <Luisscheurenbrand@gmail.com>
2020-03-21 03:57:56 +0100

    implement ipc, audio player (untested)

--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,51 @@
-#include <stratosphere.hpp>
+#include "impl/music_player.hpp"
+#include "music_control_service.hpp"
+#include "scoped_file.hpp"
 
 extern "C" {
+extern u32 __start__;
 
 u32 __nx_applet_type = AppletType_None;
+u32 __nx_fs_num_sessions = 1;
+u32 __nx_fsdev_direntry_cache_size = 1;
 
-#define INNER_HEAP_SIZE 0x8000
+#define INNER_HEAP_SIZE 0x4000
 size_t nx_inner_heap_size = INNER_HEAP_SIZE;
 char nx_inner_heap[INNER_HEAP_SIZE];
+
+void __libnx_initheap(void);
+void __appInit(void);
+void __appExit(void);
+
+/* Exception handling. */
+alignas(16) u8 __nx_exception_stack[ams::os::MemoryPageSize];
+u64 __nx_exception_stack_size = sizeof(__nx_exception_stack);
+void __libnx_exception_handler(ThreadExceptionDump *ctx);
+}
+
+namespace ams {
+
+    ncm::ProgramId CurrentProgramId = {0x4200000000000000};
+
+    namespace result {
+
+        bool CallFatalOnResultAssertion = false;
+
+    }
+
+}
+
+using namespace ams;
+
+void __libnx_exception_handler(ThreadExceptionDump *ctx) {
+    CrashHandler(ctx);
+}
 
 void __libnx_initheap(void) {
     void *addr = nx_inner_heap;
     size_t size = nx_inner_heap_size;
 
+    /* Newlib */
     extern char *fake_heap_start;
     extern char *fake_heap_end;
 
@@ -19,51 +53,61 @@ void __libnx_initheap(void) {
     fake_heap_end = (char *)addr + size;
 }
 
-void __appInit(void);
-void __appExit(void);
-}
-
 void __appInit() {
-    Result rc;
+    hos::SetVersionForLibnx();
 
-    // Initialize default services.
-    rc = smInitialize();
-    if (R_FAILED(rc))
-        fatalThrow(MAKERESULT(Module_Libnx, LibnxError_InitFail_SM));
+    sm::DoWithSession([] {
+        R_ASSERT(audoutInitialize());
+        R_ASSERT(timeInitialize());
+        R_ASSERT(fsInitialize());
+    });
 
-    // Enable this if you want to use HID.
-    /*rc = hidInitialize();
-    if (R_FAILED(rc))
-        fatalThrow(MAKERESULT(Module_Libnx, LibnxError_InitFail_HID));*/
-
-    //Enable this if you want to use time.
-    /*rc = timeInitialize();
-    if (R_FAILED(rc))
-        fatalThrow(MAKERESULT(Module_Libnx, LibnxError_InitFail_Time));
-
-    __libnx_init_time();*/
-
-    rc = fsInitialize();
-    if (R_FAILED(rc))
-        fatalThrow(MAKERESULT(Module_Libnx, LibnxError_InitFail_FS));
-
-    fsdevMountSdmc();
+    R_ASSERT(fs::MountSdCard("sdmc"));
 }
 
-void __attribute__((weak)) __appExit(void) {
-    // Cleanup default services.
-    fsdevUnmountAll();
+void __appExit(void) {
+    fs::Unmount("sdmc");
     fsExit();
-    smExit();
+    timeExit();
+    audoutExit();
 }
 
-// Main program entrypoint
+namespace {
+
+    /* music */
+    constexpr size_t NumServers = 1;
+    sf::hipc::ServerManager<NumServers> g_server_manager;
+
+    constexpr sm::ServiceName MusicServiceName = sm::ServiceName::Encode("music");
+    constexpr size_t MusicMaxSessions = 0x2;
+
+}
+
 int main(int argc, char *argv[]) {
-    // Initialization code can go here.
+    {
+        ScopedFile log("sdmc:/music.log");
+        log.WriteString("started\n");
+    }
 
-    // Your code / main loop goes here.
-    // If you need threads, you can use threadCreate etc.
+    music::Initialize();
 
-    // Deinitialization and resources clean up code can go here.
+    os::Thread audioThread;
+    audioThread.Initialize(music::ThreadFunc, nullptr, 0x32000, 0x2C);
+    audioThread.Start();
+
+    /* Create services */
+    R_ASSERT(g_server_manager.RegisterServer<music::ControlService>(MusicServiceName, MusicMaxSessions));
+
+    g_server_manager.LoopProcess();
+
+    music::Exit();
+
+    audioThread.Wait();
+    audioThread.Join();
+
+    {
+        ScopedFile log("sdmc:/music.log");
+        log.WriteString("exit\n");
+    }
     return 0;
 }
